2c60459149f2d5a2e9506a2cbabb2282b398da1f
lorinaandr <48472861+lorinaandr@users.noreply.github.com>
2020-06-22 17:05:40 +0300

    Update autentification.php

--- autentification.php
+++ autentification.php
@@ -1,106 +1,6 @@
 <?php
-
-require_once "config.php";
-// Functions to filter user inputs
-function filterName($field){
-    $field = filter_var(trim($field), FILTER_SANITIZE_STRING);
-    // Validate user name
-    if(filter_var($field, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        return $field;
-    } else{
-        return FALSE;
-    }
-}  
-function filterEmail($field){
-    // Sanitize e-mail address - sterge caracterele care nu pot fi intr-un email address
-    $field = filter_var(trim($field), FILTER_SANITIZE_EMAIL); 
-    
-    // Validate e-mail address
-    if(filter_var($field, FILTER_VALIDATE_EMAIL)){
-        return $field;
-    } else{
-        return FALSE;
-    }
-}
-
-$numeErr = $prenumeErr = $emailErr = $parolaErr = $confirmareparolaErr =  "";
-$nume = $prenume = $email = $parola = $confirmareparola = "";
-// Procesam datele formularului
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-// Validam numele
-    if(empty($_POST["nume"])){
-        $numeErr = "Va rugam sa introduceti numele dumneavoastra.";
-    } else{
-        $nume = filterName($_POST["nume"]);
-        if($nume == FALSE){
-            $numeErr = "Numele introdus nu este valid.";
-        }
-    }
-//Validam prenumele
-      if(empty($_POST["prenume"])){
-        $prenumeErr = "Va rugam sa introduceti prenumele dumneavoastra.";
-    } else{
-        $prenume = filterName($_POST["prenume"]);
-        if($prenume == FALSE){
-            $prenumeErr = "Prenumele introdus nu este valid.";
-        }
-    }
-//Validam adresa de email
-    if(empty($_POST["e-mail"])){
-        $emailErr = "Va rugam sa introduceti adresa de email.";     
-    } else{
-        $email = filterEmail($_POST["e-mail"]);
-        if($email == FALSE){
-            $emailErr = "Adresa introdusa nu este valida.";
-        }
-    }
-//Validam parola
- if(empty(trim($_POST["parola"]))){
-        $parolaErr = "Va rugam sa introduceti o parola.";     
-    } elseif(strlen(trim($_POST["parola"])) < 6){
-        $parolaErr = "Parola trebuie sa contina cel putin 6 caractere.";
-    } else{
-        $parola = trim($_POST["parola"]);
-    }
-// Validam parola confirmata
-    if(empty(trim($_POST["confirmareparola"]))){
-        $confirmareparolaErr = "Va rugam confirmati parola.";     
-    } else{
-        $confirmareparola = trim($_POST["confirmareparola"]);
-        if(empty($parolaErr) && ($parola != $confirmareparola)){
-            $confirmareparolaErr = "Confirmarea parolei a esuat.";
-        }
-
-}
-   // verificam sa vedem daca avem erori inainte de a introduce in baza de date
-    if(empty($numeErr) && empty($prenumeErr) && empty($emailErr) && empty($parolaErr)&& empty($confirmareparolaErr)){
-        
- $nume = mysqli_real_escape_string($link, $_REQUEST['nume']);
-$prenume = mysqli_real_escape_string($link, $_REQUEST['prenume']);
-$email = mysqli_real_escape_string($link, $_REQUEST['e-mail']);
-$parola = mysqli_real_escape_string($link, $_REQUEST['parola']);
-
-  $parola=password_hash($parola, PASSWORD_DEFAULT); 
-
-// Attempt insert query execution
-$sql = "INSERT INTO useri (nume, prenume, email, parola) VALUES ('$nume', '$prenume', '$email','$parola')";
-
-
-if(mysqli_query($link, $sql)){
-     header("location: autentifMsg.html");
-} else{
-    echo "ERROR: Not able to execute $sql. " . mysqli_error($link);
-}
-
-// Close connection
-mysqli_close($link);
-
-        }
-}
+require_once "form.php";
          ?>
-         
-         
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -116,14 +16,14 @@ mysqli_close($link);
   <header id="masthead" class="site-header">
         <div class="site-branding">
              <h2 class="site-title">
-               <a href="homepage.html" rel="home"> Plantonline </a>
+               <a href="homepage.php" rel="home"> Plantonline </a>
               </h2>
               <div class="site-description"> Descopera lumea plantelor </div>
         </div>
       <div class="navbar">
         <nav>
            <ul class="responsive-menu">
-           
+            <li>
               <ul class="responsive">
               <li class="lista"><a href="homepage.php"><i class="fa fa-home"></i> </a> </li>
           <li class="lista"><a href="#"><i class="fa fa-bars"></i> </a>
@@ -149,14 +49,18 @@ mysqli_close($link);
                                <li><a href="sisinei-de-munte.php">Sisinei de munte</a></li>
                             </ul>
                           </li>
+                          <li class="mnt"><a href="multicriteriala.php">Cautare multicriteriala</a>
+                          </li>
                      </ul> 
          </li>
           <li class="lista"> <a href="login.php"><i class="fa fa-sign-in"></i></a> </li>
-          <li class="lista"> <a href="statistici.html"><i class="fa fa-file-text-o"></i></a> </li>
+          <li class="lista"> <a href="autentification.php"><i class="fa fa-file-text-o"></i></a> </li>
           <li class="search-container">
+            <form action="search.php" method="POST">
             <input class="search" type="text" placeholder="Search..." name="search">
-            <button class="searchbtn" type="submit"><i class="fa fa-search"></i></button>
-          </li></ul> </ul> 
+            <button class="searchbtn" type="submit" name="submit-search"><i class="fa fa-search"></i></button>
+          </form>
+          </li></ul> </li></ul> 
                  
         <ul class="menu">
           <li class="lista"><a href="homepage.php">Acasa</a> </li>
@@ -183,13 +87,17 @@ mysqli_close($link);
                                <li><a href="sisinei-de-munte.php">Sisinei de munte</a></li>
                             </ul>
                           </li>
+                          <li class="mnt"><a href="multicriteriala.php">Cautare multicriteriala</a>
+                          </li>
                      </ul> 
          </li>
           <li class="lista"> <a href="login.php">Login</a> </li>
-          <li class="lista"> <a href="statistici.html">Statistici</a> </li>
+          <li class="lista"> <a href="autentification.php">Inregistrare</a> </li>
           <li class="search-container">
+            <form action="search.php" method="POST">
             <input class="search" type="text" placeholder="Search..." name="search">
-            <button class="searchbtn" type="submit"><i class="fa fa-search"></i></button>
+             <button class="searchbtn" type="submit" name="submit-search"><i class="fa fa-search"></i></button>
+          </form>
           </li>
       </ul>
      </nav>
@@ -204,7 +112,7 @@ mysqli_close($link);
                   <div class="titleMiddle">Inregistrare</div>
                   <form class="formular" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
                     <div class="input">
-                       <p><span class = "error">* required field.</span></p>
+                       <p><span class = "error">* campuri obligatorii.</span></p>
                       <label>Nume</label>
                       <input class="form1" type="text" name="nume" value="<?php echo $nume; ?>"> 
                       <span class="error">*  <br><?php echo $numeErr; ?> </span></div>
@@ -223,26 +131,30 @@ mysqli_close($link);
                     <div class="input">
                       <label>Confirmare parola</label>
                       <input class="form1" type="password" name="confirmareparola" value="<?php echo $confirmareparola; ?>">
-                       <span class="error">*<br><?php echo $confirmareparolaErr; ?></span></div>
-                    </div>
-
-                                      
+                       <span class="error">*<br><?php echo $confirmareparolaErr; ?></span></div>  
+                       <label for='securitate'>Intrebare de securitate:<span class="error">*<br><?php echo $securitateErr; ?></span></label>
+                      <select name='securitate' id='securitate'>
+                      <option value="">Selecteaza</option>
+                      <option value="Care este animalul preferat?">Care este animalul preferat?</option>
+                      <option value="Care este numele tatalui?">Care este numele tatalui?</option>
+                     <option value="Care este data ta de nastere?">Care este data ta de nastere?</option>
+                     </select>  <br> <br>     
+                    <input class="form1" type="text" name="raspuns" placeholder="Raspuns" > <span class="error">*<br><?php echo $raspunsErr; ?></span>
                    <div class="button">
                     <input type="submit" name="register" value="Inregistrare">
                     </div>
-                     </form>
+                     </form> </div>
 
                 </div>
                 
              </div>
              
           </div> 
+             </main>
 
   </div>
  
-      </main>
-
-    </div>
+   
 
 <div id="footer">
         
