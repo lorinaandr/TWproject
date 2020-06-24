d933679201d15becfda10522533bb49d1dadf11a
lorinaandr <48472861+lorinaandr@users.noreply.github.com>
2020-06-24 09:33:43 +0300

    Update display.php

--- display.php
+++ display.php
@@ -1,12 +1,10 @@
 <?php
     require_once 'config.php';
-    $album = " SELECT * FROM albums where id_user = '$id'";
+    $album = " SELECT * FROM albums where id_user = ?";
  $stmt= mysqli_stmt_init($link); 
-
-    if(!mysqli_stmt_prepare($stmt,$album)){
-   echo "Eroare sql";
-    }else{
-    mysqli_stmt_execute($stmt);
+if($stmt = mysqli_prepare($link, $album)){
+    mysqli_stmt_bind_param($stmt, "i",$id);
+if(mysqli_stmt_execute($stmt)){
      $result= mysqli_stmt_get_result($stmt);
    while ($row = mysqli_fetch_assoc($result)) { 
  echo "<div class='responsive-album'>";
@@ -29,26 +27,20 @@
        echo "<div class='desc'> </div>";
         echo "</div>";
       echo "</div>";
-   } } 
+   } } }
 
-    $sql =  " SELECT * FROM ierbar where id_user = '$id' and album=' ' or filetitle=' ' order by id desc; ";
-    $stmt= mysqli_stmt_init($link); 
-
-    if(!mysqli_stmt_prepare($stmt,$sql)){
-   echo "Eroare sql";
-    }else{
-    mysqli_stmt_execute($stmt);
+    $sql =  " SELECT * FROM ierbar where id_user = ? and album=' ' or filetitle=' ' order by id desc; ";
+ $stmt= mysqli_stmt_init($link); 
+if($stmt = mysqli_prepare($link, $sql)){
+    mysqli_stmt_bind_param($stmt, "i",$id);
+if(mysqli_stmt_execute($stmt)){
      $results= mysqli_stmt_get_result($stmt);
    while ($row = mysqli_fetch_assoc($results)) {
       echo "<div class='responsive-album'>";
       echo "<div class='gallery'>";
       echo "<div class='desc'> </div>";
-     // echo "<a href='../projectTW/gallery/".$row['file']."'>";
         echo "<img src='../projectTW/gallery/".$row['file']. "' alt='".$row['filetitle']."' title='".$row['filetitle']."'>";
-      //  echo "<form action='delete-images.php?album=".$row['album']."&id=".$row['id']."&file=".$row['file']."' method='POST'>";
         echo "  <button id='sterge-poza' type='submit' name='sterge-poza' onclick='delete_image(".$row['id'].")'>Sterge</button> ";
-      //  echo"</form>";
-       // echo "</a>";
         echo "<div class='desc'> ".$row['filedesc']." </div>";
         echo "<div class='hide'>";
         echo "<form action='delete-images.php?id=".$row['id']."&file=".$row['file']."' method='POST'>";
@@ -71,6 +63,6 @@
         echo "</div>";
       echo "</div>";
     }
-  }
+  } }
   mysqli_close($link);
   ?>
